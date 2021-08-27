bd51cce34e422a628cd43396df9b7379d7f04bca
Kai D. Gonzalez <gkai70264@gmail.com>
2021-08-27 11:09:21 -0400

    add extras (auto)

--- SDKL/browser/src/checkbuild.sh
+++ SDKL/browser/src/checkbuild.sh
@@ -1,5 +1,5 @@
 sudo rm /usr/bin/sdkl
 sudo rm /usr/bin/sdkl-bytes
 
-sudo ln ./sdkl /usr/bin/sdkl
-sudo ln ./sdkl-bytes /usr/bin/sdkl-bytes
+sudo ln ./sdkl /usr/bin/sdkl -s
+sudo ln ./sdkl-bytes /usr/bin/sdkl-bytes -s
